fea4396fcfbe4772930b19b7c6ee5bd8e3ef3cc3
jasongama <jasongama967@gmail.com>
2019-07-31 17:14:05 +0200

    done

--- js/colors.dom.js
+++ js/colors.dom.js
@@ -1,7 +1,9 @@
 const addBtn = document.querySelector(".addBtn");
 const filterBtn = document.querySelector(".filterBtn");
 const errorMessage = document.querySelector(".error");
-
+const shapes= document.querySelector(".shapes");
+const selectColor=document.querySelector(".selectColor");
+const selectNumber= document.querySelector(".selectNumber")
 function clearError() {
 	setTimeout(function(){
 		errorMessage.innerHTML = "";
@@ -9,6 +11,34 @@ function clearError() {
 }
 
 addBtn.addEventListener("click", function(){
+
+	// alert()
+	const currentColor =selectColor.value;
+//   shapes.innerHTML= "No shapes added yet.";
+ //create a new div element
+ const newShape = document.createElement("div");
+ newShape.classList.add("shape");
+ newShape.classList.add("square");
+ newShape.classList.add("blue");
+ newShape.classList.add(currentColor)
+
+ const currentNumber = selectNumber.value;
+
+ if(currentColor === "" || currentNumber ===""){
+	 errorMessage.innerHTML ="Select a color & number"
+	 clearError();
+	 return;
+ }
+
+const newNumber= document.createElement("div")
+newNumber.innerHTML = currentNumber
+ newNumber.classList.add("number")
+ newNumber.classList.add(currentNumber)
+
+ newShape.appendChild(newNumber);
+
+
+ shapes.appendChild(newShape)
 
 	errorMessage.innerHTML = "Add!"
 	clearError();
